5196d039904f1afcf5a1e246482c44fa8bb13103
halcyondr <53500920+halcyondr@users.noreply.github.com>
2022-05-04 20:42:57 -0400

    Update _config.tf

--- project/starter-code/zone2/_config.tf
+++ project/starter-code/zone2/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-travis-west"
+     bucket = "udacity-tf-raviw1"
      key    = "terraform/terraform.tfstate"
      region = "us-west-1"
    }
