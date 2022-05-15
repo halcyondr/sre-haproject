bbb4fe8a089cf456384606ebe6ac6b7bdbd2b704
halcyondr <53500920+halcyondr@users.noreply.github.com>
2022-05-15 13:36:10 -0400

    Update rds.tf

--- project/starter-code/zone1/modules/rds-s/rds.tf
+++ project/starter-code/zone1/modules/rds-s/rds.tf
@@ -35,7 +35,8 @@ resource "aws_rds_cluster" "udacity_cluster-s" {
   backup_retention_period  = 5
   replication_source_identifier   = var.primary_db_cluster_arn
   source_region            = "us-east-2"
-  depends_on = [aws_rds_cluster_parameter_group.cluster_pg-s]
+  #depends_on = [aws_rds_cluster_parameter_group.cluster_pg-s]
+  depends_on =[var.primary_db_cluster_instance_arn, aws_rds_cluster_parameter_group.cluster_pg-s]
 }
 
 resource "aws_rds_cluster_instance" "udacity_instance-s" {
