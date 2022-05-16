112c2c615ce18a5a3c31576c3e0d9d0b0125a002
halcyondr <53500920+halcyondr@users.noreply.github.com>
2022-05-16 11:45:56 -0400

    Update rds.tf

--- project/starter-code/zone1/modules/rds-s/rds.tf
+++ project/starter-code/zone1/modules/rds-s/rds.tf
@@ -1,5 +1,4 @@
 variable primary_db_cluster_arn {}
-variable primary_db_cluster_instance_arn {}
 
 resource "aws_rds_cluster_parameter_group" "cluster_pg-s" {
   name   = "udacity-pg-s"
@@ -25,7 +24,7 @@ resource "aws_db_subnet_group" "udacity_db_subnet_group" {
 
 resource "aws_rds_cluster" "udacity_cluster-s" {
   cluster_identifier       = "udacity-db-cluster-s"
-  availability_zones       = ["us-west-1a","us-west-1c"]
+  availability_zones       = ["us-west-1b","us-west-1c"]
   db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.cluster_pg-s.name
   vpc_security_group_ids   = [aws_security_group.db_sg_2.id]
   db_subnet_group_name     = aws_db_subnet_group.udacity_db_subnet_group.name
@@ -34,10 +33,10 @@ resource "aws_rds_cluster" "udacity_cluster-s" {
   skip_final_snapshot      = true
   storage_encrypted        = false
   backup_retention_period  = 5
-  replication_source_identifier   = var.primary_db_cluster_arn
+  replication_source_identifier = var.primary_db_cluster_arn
   source_region            = "us-east-2"
   #depends_on = [aws_rds_cluster_parameter_group.cluster_pg-s]
-  depends_on =[var.primary_db_cluster_instance_arn, aws_rds_cluster_parameter_group.cluster_pg-s]
+  depends_on = [aws_rds_cluster_parameter_group.cluster_pg-s]
 }
 
 resource "aws_rds_cluster_instance" "udacity_instance-s" {
